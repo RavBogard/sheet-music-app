a123a8464752ef74bfe52ce459a8939dba346bd8
Synagogue Musician <musician@example.com>
2026-01-28 16:53:01 -0600

    fix(next): add google-cloud/vision to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@google-cloud/vision'],
   images: {
     remotePatterns: [
       {
